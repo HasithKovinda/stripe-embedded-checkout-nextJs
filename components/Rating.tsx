e7d58db2992d3216b0c2232af694eefac492df44
HasithKovinda <hasithkovinda@outlook.com>
2024-05-11 15:40:41 +0530

    design single product component

---
+++ components/Rating.tsx
@@ -0,0 +1,40 @@
+import styles from "./TourRating.module.css";
+
+type TourRatingProp = {
+  ratingNumber: number;
+};
+
+export default function Rating({ ratingNumber }: TourRatingProp) {
+  const widthSize = Number(
+    (ratingNumber - Math.floor(ratingNumber)).toFixed(1)
+  );
+  const applyWith = widthSize === 0 ? 30 : widthSize * 3 * 10;
+  const gap = 1 - widthSize;
+  return (
+    <div className="section">
+      {Array.from({ length: 5 }, (_, i) => {
+        if (ratingNumber >= i + 1) {
+          return (
+            <div className="review-container" key={i}>
+              <div className="progress" style={{ width: `30px` }}></div>
+              <img src="/download.png" className="icon" />
+            </div>
+          );
+        } else {
+          return (
+            <div className="review-container" key={i}>
+              <div
+                className="progress"
+                style={{
+                  width:
+                    ratingNumber + gap === i + 1 ? `${applyWith}px` : `0px`,
+                }}
+              ></div>
+              <img src="/download.png" className="icon" />
+            </div>
+          );
+        }
+      })}
+    </div>
+  );
+}
